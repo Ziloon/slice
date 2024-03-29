c2a7302e366ebeda36ffd523e5c5fb1bdd7e2410
Zhao Ziloong <ziloon.z@icloud.com>
2024-03-30 00:38:30 +0800

    其他切片类型

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,10 @@ fn main() {
     let word1 = second_word(&s[..]);
     println!("{}", word1);
     // s.clear();
+
+    let a = [1, 2, 3, 4, 5];
+    let slice = &a[1..3];
+    assert_eq!(slice, &[2, 3]);
 }
 
 fn first_word(s: &str) -> &str {
