9558dfbeacc058d1e01ecb2caf9cc077fb4dfef0
Zhao Ziloong <ziloon.z@icloud.com>
2024-03-30 00:03:28 +0800

    谁用字符串切片作为返回值

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,27 @@
 fn main() {
-    let mut s = String::from("Hello, world!");
+    let s = String::from("Hello world");
+    let _hello = &s[..5];
+    let _world = &s[6..];
+    let _whole = &s[..];
+
     let word = first_word(&s);
     println!("{}", word);
-    let (start, end) = second_word(&s);
-    println!("{} {}", start, end);
-    s.clear();
+    let word1 = second_word(&s);
+    println!("{}", word1);
+    // s.clear();
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes();
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[..i];
         }
     }
-    s.len()
+    &s[..]
 }
 
-fn second_word(s:&String) -> (usize, usize) {
+fn second_word(s:&String) -> &str {
     let bytes = s.as_bytes();
     let mut l:[usize;2] = [0;2];
     let mut idx = 0;
@@ -26,11 +30,11 @@ fn second_word(s:&String) -> (usize, usize) {
             l[idx] = i;
             idx += 1;
             if idx >= l.len() {
-                return (l[0], l[1]);
+                return &s[l[0]..l[1]];
             }
             continue;
         }
     }
     l[idx] = s.len();
-    (l[0], l[1])
+    &s[l[0]..l[1]]
 }
